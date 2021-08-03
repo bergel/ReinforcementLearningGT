95aa137f7c26eb1aaad55f1ca1693fe5ce7865cb
Alexandre Bergel <abergel@dcc.uchile.cl>
2021-08-03 17:00:00 +0200

    Update runExamples.sh

--- scripts/runExamples.sh
+++ scripts/runExamples.sh
@@ -4,6 +4,12 @@ cd glamoroustoolkit
 cp ../scripts/runExamples.st .
 ./glamoroustoolkit --headless GlamorousToolkit.image runExamples.st
 
+if [ ! -f /tmp/result.txt ]
+then
+    echo "Result file not generated"
+    exit 1
+fi
+
 cat /tmp/result.txt
 if grep -q Error "/tmp/result.txt"; then
 	echo "Errors: Some tests failed"
